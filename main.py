1f214ee9bfe0a0dbcdf11c5ec844cda408379b4a
Magikarp4000 <1fishyblub1@gmail.com>
2024-11-02 20:21:54 +0000

    Cleaned up main.py code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from config import *
-
 import pygame
 from pygame.locals import *
 
 import random
-import player
+import math
+
+from config import *
+from Player import Player
 from Spritesheet import Spritesheet
 from scale import scale_image
-import math
 
 
 pygame.init()
@@ -20,7 +20,7 @@ music.play(-1)
 def scale(arr, k):
     return [x * k for x in arr]
 
-def get_input(pressed):
+def get_camera_input(pressed):
     dx, dy = 0, 0
     if pressed[K_RIGHT] or pressed[K_d]:
         dx += SPEED
@@ -35,13 +35,9 @@ def get_input(pressed):
         return dx, dy
     return dx / mag * SPEED, dy / mag * SPEED
 
-def main():
-    running = True
+def gen_world():
     tilesheet = Spritesheet('assets/texture/TX Tileset Grass.png', 16)
-    camera_x = WORLD_WIDTH / 2
-    camera_y = WORLD_HEIGHT / 2
     tiles = {}
-    player1 = player.Player()
     for i in range(WORLD_HEIGHT):
         for j in range(WORLD_WIDTH):
             x, y = 12, 14
@@ -50,25 +46,45 @@ def main():
                 y = random.randint(0, tilesheet.h - 1)
             tile = scale_image(tilesheet.get_image(x, y), TILE_SIZE)
             tiles[i, j] = tile
+    return tiles
+
+def render_tiles(tiles, camera_x, camera_y):
+    for x in range(int(camera_x), int(camera_x) + WIDTH + 1):
+        for y in range(int(camera_y), int(camera_y) + HEIGHT + 1):
+            pos_x = (x - camera_x) * TILE_SIZE
+            pos_y = (y - camera_y) * TILE_SIZE
+            screen.blit(tiles[y, x], tiles[y, x].get_rect(topleft=(pos_x, pos_y)))
+
+def main():
+    camera_x = WORLD_WIDTH / 2
+    camera_y = WORLD_HEIGHT / 2
+
+    tiles = gen_world()
+    sprites = pygame.sprite.Group()
+
+    player = Player()
+    sprites.add(player)
+
+    running = True
     while running:
-        keys = pygame.key.get_pressed()
-        player1.move(keys)
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
+        
         pressed = pygame.key.get_pressed()
-        dx, dy = get_input(pressed)
+        player.move(pressed)
+        dx, dy = get_camera_input(pressed)
+
         camera_x = min(WORLD_WIDTH - WIDTH - 1, max(0, camera_x + dx))
         camera_y = min(WORLD_HEIGHT - HEIGHT - 1, max(0, camera_y + dy))
-        for x in range(int(camera_x), int(camera_x) + WIDTH + 1):
-            for y in range(int(camera_y), int(camera_y) + HEIGHT + 1):
-                pos_x = (x - camera_x) * TILE_SIZE
-                pos_y = (y - camera_y) * TILE_SIZE
-                screen.blit(tiles[y, x], tiles[y, x].get_rect(topleft=(pos_x, pos_y)))
-        player1.update()
-        screen.blit(player1.image,player1.rect)
+
+        render_tiles(tiles, camera_x, camera_y)
+        sprites.update()
+
+        screen.blit(player.image, player.rect)
         pygame.display.flip()
         clock.tick(FPS)
+
     pygame.quit()
 
 
